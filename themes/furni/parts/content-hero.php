2537d75856aef065722d90f2ab6b4c6387932a40
81joker <nehad.al.timimi@gmail.com>
2024-11-20 20:56:49 +0100

    add images + made header + footer

---
+++ themes/furni/parts/content-hero.php
@@ -0,0 +1,21 @@
+	<!-- Start Hero Section -->
+	<div class="hero">
+	    <div class="container">
+	        <div class="row justify-content-between">
+	            <div class="col-lg-5">
+	                <div class="intro-excerpt">
+	                    <h1><?php echo the_title() ?></h1>
+	                    <!-- <h1>Modern Interior <span clsas="d-block">Design Studio</span></h1> -->
+	                    <p class="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.</p>
+	                    <p><a href="<?php echo get_permalink(10); ?>" class="btn btn-secondary me-2">Shop Now</a><a href="#" class="btn btn-white-outline">Explore</a></p>
+	                </div>
+	            </div>
+	            <div class="col-lg-7">
+	                <div class="hero-img-wrap">
+	                    <img src="<?php echo get_theme_file_uri('images/couch.png') ?>" class="img-fluid">
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+	<!-- End Hero Section -->
--- themes/furni/parts/content-navbar.php
+++ themes/furni/parts/content-navbar.php
@@ -2,7 +2,7 @@
 		<nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
 			<div class="container">
-				<a class="navbar-brand" href="index.html">Furni<span>.</span></a>
+				<a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">Furni<span>.</span></a>
 
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
@@ -10,22 +10,43 @@
 
 				<div class="collapse navbar-collapse" id="navbarsFurni">
 					<ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-						<li class="nav-item active">
-							<a class="nav-link" href="index.html">Home</a>
+						<li class="nav-item <?php if (is_page('home') || wp_get_post_parent_id(0) == 16) echo 'active'; ?>">
+
+							<a class="nav-link " href="<?php echo esc_url(home_url('/')); ?>">
+								Home
+							</a>
+
 						</li>
-						<li><a class="nav-link" href="shop.html">Shop</a></li>
-						<li><a class="nav-link" href="about.html">About us</a></li>
-						<li><a class="nav-link" href="services.html">Services</a></li>
-						<li><a class="nav-link" href="blog.html">Blog</a></li>
+
+						<li class="nav-item <?php if (is_page('shop') || wp_get_post_parent_id(0) == 16) echo 'active'; ?>">
+							<a class="nav-link " href="<?php echo site_url('/shop'); ?>">
+								Shop
+							</a>
+						</li>
+						<li class="nav-item <?php if (is_page('about') || wp_get_post_parent_id(0) == 16) echo 'active'; ?>">
+							<a class="nav-link " href="<?php echo site_url('/about'); ?>">
+								About
+							</a>
+						</li>
+						<li class="nav-item <?php if (is_page('services') || wp_get_post_parent_id(0) == 16) echo 'active'; ?>">
+							<a class="nav-link " href="<?php echo site_url('/services'); ?>">
+							Services
+							</a>
+						</li>
+						<li class="nav-item <?php if (is_page('blog') || wp_get_post_parent_id(0) == 16) echo 'active'; ?>">
+							<a class="nav-link " href="<?php echo site_url('/blog'); ?>">
+							Blog
+							</a>
+						</li>
 						<li><a class="nav-link" href="contact.html">Contact us</a></li>
 					</ul>
 
 					<ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-						<li><a class="nav-link" href="#"><img src="images/user.svg"></a></li>
-						<li><a class="nav-link" href="cart.html"><img src="images/cart.svg"></a></li>
+						<li><a class="nav-link" href="#"><img src="<?php echo get_theme_file_uri('images/user.svg') ?>"></a></li>
+						<li><a class="nav-link" href="cart.html"><img src="<?php echo get_theme_file_uri('images/cart.svg') ?>"></a></li>
 					</ul>
 				</div>
 			</div>
-				
+
 		</nav>
 		<!-- End Header/Navigation -->
